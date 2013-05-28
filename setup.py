434efcd0aca196a708ece7c3fae14a0d66b4e566
Aleksey Komissarov <ad3002@gmail.com>
2013-05-28 10:20:12 +0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='PyExp',
+    version='0.1.0',
+    author='Aleksey Komissarov',
+    author_email='ad3002@gmail.com',
+    packages=['PyExp', 'PyExp.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/PyExp/',
+    license='LICENSE',
+    description='A microframework for small computational experiments.',
+    long_description=open('README').read(),
+    install_requires=[
+
+    ],
+)
