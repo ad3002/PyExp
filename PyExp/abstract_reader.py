32f1c9be157ea0115531b7f681b698868dbb7f6e
Aleksey Komissarov <ad3002@gmail.com>
2015-04-17 20:21:30 +0400

    minor fixed and archive opener

--- PyExp/abstract_reader.py
+++ PyExp/abstract_reader.py
@@ -7,6 +7,7 @@
 """
     Classes:
     
+    - WiseOpener(object)
     - AbstractFileIO(object)
     - AbstractFolderIO(object)
     - AbstractFoldersIO(object)
@@ -25,35 +26,41 @@ import pickle
 import shutil
 import gzip
 import bz2
+from PyExp import exp_logger
 
-class WizeOpener(object):
-    ''' Opener to open usual files and gzip archives.
-    '''   
+
+class WiseOpener(object):
+    """ Opener to open usual files and gzip or bzip archives.
+    """ 
+      
     def __init__(self, file_name, mode=None):
         self.file_name = file_name
         if not mode:
             mode = "r"
-        assert mode in ["w", "r", "a", "wb", "rb", "ab"]
+        if not mode in ["w", "r", "a", "wb", "rb", "ab"]:
+            exp_logger.error("Wrong file mode: %s" % mode)
+            raise Exception("Wrong file mode: %s" % mode)
         self.mode = mode
+        self.fh = None
     
     def __enter__(self):
         if self.file_name.endswith(".gz"):
-            print "Open as gz archive"
+            exp_logger.info("Open as gz archive")
             if not "b" in self.mode:
                 self.mode += "b"
             self.fh = gzip.open(self.file_name, self.mode)
         elif self.file_name.endswith(".bz2"):
-            print "Open as bz2 archive"
+            exp_logger.info("Open as bz2 archive")
             if not "b" in self.mode:
                 self.mode += "b"
             self.fh = bz2.BZ2File(self.file_name, self.mode)
         else:
             self.fh = open(self.file_name, self.mode)
         return self.fh
-    
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, *args):
         self.fh.close()
+
 
 class AbstractFileIO(object):
     """ Abstract class for working with abstract data.
@@ -85,16 +92,16 @@ class AbstractFileIO(object):
         self._data = None
 
     def get_opener(self):
-        return WizeOpener
+        return WiseOpener
 
     def read_from_file(self, input_file):
         """ Read data from given input_file."""
-        with WizeOpener(input_file) as fh:
+        with WiseOpener(input_file) as fh:
             self._data = fh.readlines()
 
     def read_online(self, input_file):
         """ Yield items from data online from input_file."""
-        with WizeOpener(input_file) as fh:
+        with WiseOpener(input_file) as fh:
             for item in fh:
                 yield item
 
@@ -114,7 +121,7 @@ class AbstractFileIO(object):
             for x in cursor[start:end]:
                 yield x
             start = end
-            end = end + limit
+            end += limit
             if start > n:
                 break
 
@@ -123,7 +130,7 @@ class AbstractFileIO(object):
 
     def write_to_file(self, output_file):
         """ Write data to given output_file."""
-        with WizeOpener(output_file, "w") as fh:
+        with WiseOpener(output_file, "w") as fh:
             fh.writelines(self._data)
 
     def write_to_db(self, db_cursor):
@@ -198,6 +205,7 @@ class AbstractFileIO(object):
     def N(self):
         return len(self._data)
 
+
 class AbstractFolderIO(object):
     """ Abstract class for working with abstract data in folder.
     
@@ -268,16 +276,18 @@ class AbstractFolderIO(object):
             for name in files:
                 if re.search(self.mask, name):
                     path = os.path.join(root, name)
-                    with WizeOpener(path, "rb") as fh:
+                    with WiseOpener(path, "rb") as fh:
                         yield fh.read()
 
     def iter_file_content_and_names(self):
-        """ iter over files in folder. Return file content, file_name, file_path."""
+        """
+        Iterate over files in folder. Return file content, file_name, file_path.
+        """
         for root, dirs, files in os.walk(self.folder, topdown=False):
             for name in files:
                 if re.search(self.mask, name):
                     path = os.path.join(root, name)
-                    with WizeOpener(path, "rb") as fh:
+                    with WiseOpener(path, "rb") as fh:
                         yield fh.read(), name, path
 
     def move_files_by_mask(self, dist_folder):
@@ -297,9 +307,11 @@ class AbstractFolderIO(object):
                 os.remove(dist_file)
             shutil.copy2(file_path, dist_file)
 
+
 class AbstractFoldersIO(AbstractFileIO):
     """ Abstract class for working with abstract data in folder of folders."""
     pass
+
 
 def sc_iter_filepath_folder(folder, mask="."):
     """ Shortcut for iterating file path in given folder."""
@@ -307,11 +319,13 @@ def sc_iter_filepath_folder(folder, mask="."):
     for path in reader.iter_filenames():
         yield path
 
+
 def sc_iter_filename_folder(folder, mask="."):
     """ Shortcut for iterating filename in given folder."""
     reader = AbstractFolderIO(folder, mask=mask)
     for filename in reader.iter_files():
         yield filename
+
 
 def sc_iter_path_name_folder(folder, mask="."):
     """ Shortcut for iterating (filename, path) in given folder."""
@@ -319,16 +333,19 @@ def sc_iter_path_name_folder(folder, mask="."):
     for filename, path in reader.iter_path_names():
         yield filename, path
 
+
 def sc_iter_filedata_folder(folder, mask="."):
     """ Shortcut for iterating file content in given folder."""
     reader = AbstractFolderIO(folder, mask=mask)
     for data in reader.iter_file_content():
         yield data
 
+
 def sc_move_files(folder, dist_folder, mask="."):
     """ Shortcut for moving file from folder to dist."""
     reader = AbstractFolderIO(folder, mask=mask)
     reader.move_files_by_mask(dist_folder)
+
 
 def sc_process_file(file_name, cf, args_dict):
     """ Shortcut for processing file
@@ -337,8 +354,9 @@ def sc_process_file(file_name, cf, args_dict):
     reader.read(file_name)
     args_dict["name"] = name
     text = cf(text, **args_dict)
-    with WizeOpener(file_name, "w") as fh:
+    with WiseOpener(file_name, "w") as fh:
         fh.write(text)
+
 
 def sc_process_folder(folder, cf, args_dict, mask="."):
     """ Shortcut for processing each file in folder
@@ -347,8 +365,9 @@ def sc_process_folder(folder, cf, args_dict, mask="."):
     for text, name, file_name in reader.iter_file_content_and_names():
         args_dict["name"] = name
         text = cf(text, **args_dict)
-        with WizeOpener(file_name, "w") as fh:
+        with WiseOpener(file_name, "w") as fh:
             fh.write(text)
+
 
 def sc_process_folder_to_other(folder, output_folder, cf, args_dict, mask=".", verbose=False):
     """ Shortcut for processing each file in folder
@@ -365,12 +384,13 @@ def sc_process_folder_to_other(folder, output_folder, cf, args_dict, mask=".", v
         text = cf(text, **args_dict)
         output_file = os.path.join(output_folder,
                                    name)
-        with WizeOpener(output_file, "w") as fh:
+        with WiseOpener(output_file, "w") as fh:
             fh.write(text)
 
+
 def read_pickle_file(pickle_file):
-    ''' Read pickle file and retrun its content.
-    '''
-    with WizeOpener(pickle_file, "r") as fh:
+    """ Read pickle file and retrun its content.
+    """
+    with WiseOpener(pickle_file, "r") as fh:
         data = pickle.load(fh)
     return data
